5b30dc613e13628f119d3f865a955e5ec08f8dc3
David De Stefani <ddst1701@gmail.com>
2021-02-10 18:55:08 +0100

    Prominia stil i header.Napravia filpabilne kartice i sredia sitne greske...jos se triba popravit sticky nav bar

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//STICKY NAV BAR
 window.onscroll = function() {myFunction()};
 
 var nav = document.getElementsByClassName("nav")[0];
@@ -10,3 +11,12 @@ function myFunction() {
     nav.classList.remove("sticky");
   }
 }
+
+//FLIPANJE KARTICA
+
+const karte = document.querySelectorAll(".kartica");
+
+function flip() {
+  this.classList.toggle("flip");
+}
+karte.forEach((karta) => karta.addEventListener("click", flip));
