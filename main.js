0b4397020f411d06fb282553a93cfe990829ee1f
unknown <ddst1701@gmail.com>
2021-03-31 15:12:45 +0200

    napravia malo responzivnije kartice

--- main.js
+++ main.js
@@ -18,5 +18,14 @@ const linkflip = document.querySelectorAll(".linkflip");
 
 function flip() {
   this.parentElement.parentElement.parentElement.classList.toggle("flip");
+  
 }
 linkflip.forEach((link) => link.addEventListener("click", flip));
+
+const linkflip2 = document.querySelectorAll(".linkflip2");
+
+function flip2() {
+  this.parentElement.parentElement.classList.toggle("flip");
+  
+}
+linkflip2.forEach((link2) => link2.addEventListener("click", flip2));
