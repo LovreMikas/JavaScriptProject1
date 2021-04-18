14a030a54dfd6770fb6544533f2ca697e48e10c4
unknown <ddst1701@gmail.com>
2021-04-18 03:45:22 +0200

    brat lovre i stefan idu lec u 4ujutro jer su: risili dodavanje kartica samo ima mali problem, nadodali za u kosaricu ubacivat i tonu brojaca i funkcija napravili

--- main.js
+++ main.js
@@ -17,87 +17,69 @@ function flip2() {
 }
 linkflip2.forEach((link2) => link2.addEventListener("click", flip2));
 
-//STVARANJE KARTICA
+//DODAVANJE U KOSARU
 
-class kartica{
-  constructor (ime,vrime,veg,kalorije,slika){
-      var kartica = document.createElement('div');
-      kartica.classList.add('kartica');
-      document.getElementsByClassName("sredina")[0].insertBefore(kartica, document.getElementById("dodaj"));
-        //PREDNJA KARTICA
-        var prednji = document.createElement('div');
-        prednji.classList.add('prednji');
-        kartica.append(prednji);
-          /*----------------------------------------------*/
-            //PREDNJA SLIKA DIV
-            var slika = document.createElement('div');
-            slika.classList.add('slika');
-            prednji.append(slika);
-    	        /*----------------------------------------------*/
-                //SLIKA
-                var img = document.createElement('img');
-                img.setAttribute("src",slika);
-                slika.append(img);
+window.sveukupno=0
 
-            //PREDNJI TEKST
-            var tekst = document.createElement('div');
-            tekst.classList.add('tekst');
-            prednji.append(tekst);
-              /*-------------------------------------------------*/
-                //PREDNJI TEKST NAZIV
-                var naziv = document.createElement('h3');
-                naziv.classList.add('naziv');
-                naziv.append(ime);
-                tekst.append(naziv);
-                //DIV IKONE
-                var ikone = document.createElement('div');
-                ikone.classList.add('ikone');
-                tekst.append(ikone);
-                /*--------------------------------------*/
-                  //P ZA IKONE
-                  var opis = document.createElement('p');
-                  opis.classList.add('opis');
-                  ikone.append(opis);
-                  //IKONE
-                  opis.innerHTML = "<i class='far fa-clock'></i>"
-                  var vrimetxt = vrime + "min";
-                  opis.append(vrimetxt);
-                  var br = document.createElement("br");
-                  opis.append(br);
+const kosara = document.querySelectorAll(".ukosaru");
+function u_kosaru(event){
+  var koliko=1;
+  const skuhaj = event.currentTarget;
+  skuhaj.style.display="none";
+  var kontejner =skuhaj.parentElement;
+  console.log(skuhaj.parentElement);
+  //MAKNI
+  var k_botun = document.createElement('button');
+  k_botun.classList.add("nadodaj");
+  k_botun.type="button";
+  k_botun.innerHTML="-";
+  skuhaj.parentElement.insertBefore(k_botun, kontejner.lastChild);
+  k_botun.addEventListener("click",makni);
 
-                  opis.innerHTML += "<i class='fas fa-seedling'></i>"
-                  var vege;
-                  if(veg){
-                    vege = "Vegetartian"
-                  }
-                  else{
-                    vege = "Non Vegetarian"
-                  };
-                  opis.append(vege);
-                  opis.append(br);
+  //KOLICINA
+  var kolicina = document.createElement("span");
+  kolicina.innerHTML=koliko;
+  skuhaj.parentElement.insertBefore(kolicina, kontejner.lastChild);
+  kolicina.classList.add("kolicina")
 
-                  opis.innerHTML += "<i class='fab fa-gripfire'></i>"
-                  opis.append(kalorije+"kJ");
-                  opis.append(br);
-            //PREDNJI BOTUN
-            var div_botun = document.createElement('div');
-            div_botun.classList.add('botun');
-            prednji.append(div_botun);
-            
-            var botun = document.createElement('button');
-            botun.type="submit";
-            botun.innerHTML="U kosaru";
-            div_botun.append(botun);
-          
+
+  //NADODAJ
+  var k_botun2 = document.createElement('button');
+  k_botun2.classList.add("nadodaj");
+  k_botun2.type="button";
+  k_botun2.innerHTML="+";
+  skuhaj.parentElement.insertBefore(k_botun2, kontejner.lastChild);
+  k_botun2.addEventListener("click",nadodaj);
+
+  function nadodaj(){
+    koliko++;
+    kolicina.innerHTML=koliko;
+    sveukupno++;
+
   }
+  
+  function makni(){
+    koliko--;
+    kolicina.innerHTML=koliko;
+    sveukupno--;    
+    if(koliko==0){
+      skuhaj.style.display="inline-block";
+      k_botun.style.display="none";
+      k_botun2.style.display="none";
+      kolicina.style.display="none";
+    }
+  }
+  
 }
+
+kosara.forEach((kosaru) => kosaru.addEventListener("click",u_kosaru));
 
 
 //DODAVANJE NOVIH
-var brojac = 0;
+window.ogranicenje = 0;
 function dodavanje(){
-  if (brojac <= 4 ){
-      brojac++;
+  if (ogranicenje <= 4 ){
+      ogranicenje++;
       class inputkartica{
         constructor (){
           var kartica = document.createElement('div');
@@ -116,7 +98,7 @@ function dodavanje(){
                 prednji.append(slika);
                   /*----------------------------------------------*/
                     //SLIKA
-                    var i_img = document.createElement('input');//------------>
+                    window.i_img = document.createElement('input');//------------>
                     i_img.setAttribute("type", "text");
                     i_img.setAttribute("placeholder", "Url slike");
                     slika.append(i_img);
@@ -127,7 +109,7 @@ function dodavanje(){
                 prednji.append(tekst);
                   /*-------------------------------------------------*/
                     //PREDNJI TEKST NAZIV
-                    var i_naziv = document.createElement('input');//--------->
+                    window.i_naziv = document.createElement('input');//--------->
                     i_naziv.setAttribute("type", "text");
                     i_naziv.setAttribute("placeholder", "Naziv jela");
                     tekst.append(i_naziv);
@@ -141,7 +123,7 @@ function dodavanje(){
                       opis.classList.add('opis');
                       ikone.append(opis);
                       //IKONE
-                      var i_vrime = document.createElement("input");//------>
+                      window.i_vrime = document.createElement("input");//------>
                       i_vrime.setAttribute("type", "text");
                       i_vrime.setAttribute("placeholder", "Vrime kuhanja(min)");
                       opis.innerHTML = "<i class='far fa-clock'></i>";
@@ -149,7 +131,7 @@ function dodavanje(){
                       opis.append(i_vrime);
                       opis.append(br);
   
-                      var i_vege = document.createElement("input");//------>
+                      window.i_vege = document.createElement("input");//------>
                       i_vege.setAttribute("type", "text");
                       i_vege.setAttribute("placeholder", "Vegetarijansko (da/ne)");
                       opis.innerHTML += "<i class='fas fa-seedling'></i>";
@@ -157,7 +139,7 @@ function dodavanje(){
                     
                       opis.append(br);
 
-                      var i_kalorije = document.createElement("input");//------>
+                      window.i_kalorije = document.createElement("input");//------>
                       i_kalorije.setAttribute("type", "text");
                       i_kalorije.setAttribute("placeholder", "Kalorije(kJ)");
                       opis.innerHTML += "<span><i class='fab fa-gripfire'></i></span>"
@@ -168,21 +150,122 @@ function dodavanje(){
                 i_div_botun.classList.add('botun');
                 prednji.append(i_div_botun);
               
+                //IZBRISI
                 var i_botun = document.createElement('button');
                 i_botun.classList.add("potvrdi");
-                i_botun.type="submit";
-                i_botun.innerHTML="-";
+                i_botun.type="button";
+                i_botun.innerHTML="Odbaci";
                 i_div_botun.append(i_botun);
+                this.izbrisi = this.izbrisi.bind(this);
+                i_botun.addEventListener("click",this.izbrisi);
               
+                //POTVRDI
                 var i_botun2 = document.createElement('button');
                 i_botun2.classList.add("potvrdi");
-                i_botun2.type="submit";
-                i_botun2.innerHTML="+";
+                i_botun2.type="button";
+                i_botun2.innerHTML="Potvrdi";
                 i_div_botun.append(i_botun2);
+                this.potvrdi = this.potvrdi.bind(this);
+                i_botun2.addEventListener("click",this.potvrdi);
+
+                
+
             
         }
+        izbrisi(event){
+          var odabrani = event.currentTarget;
+          odabrani.parentElement.parentElement.parentElement.parentElement.style.display="none";
+          ogranicenje--;
+        }
+        potvrdi(event){
+          //STVARANJE KARTICA
+          console.log("ajboaj2");
+          var v_naziv=i_naziv.value;
+          var v_vrime=i_vrime.value;
+          var v_vege=i_vege.value;
+          var v_kalorije=i_kalorije.value;
+          var v_img=i_img.value;
+          class kartica{
+            constructor (ime,vrime,veg,kalorije,foto){
+                var kartica = document.createElement('div');
+                kartica.classList.add('kartica');
+                document.getElementsByClassName("sredina")[0].insertBefore(kartica, document.getElementById("dodaj"));
+                  //PREDNJA KARTICA
+                  var prednji = document.createElement('div');
+                  prednji.classList.add('prednji');
+                  kartica.append(prednji);
+                    /*----------------------------------------------*/
+                      //PREDNJA SLIKA DIV
+                      var slika = document.createElement('div');
+                      slika.classList.add('slika');
+                      prednji.append(slika);
+                        /*----------------------------------------------*/
+                          //SLIKA
+                          var img = document.createElement('img');
+                          img.setAttribute("src",foto);
+                          slika.append(img);
+
+                      //PREDNJI TEKST
+                      var tekst = document.createElement('div');
+                      tekst.classList.add('tekst');
+                      prednji.append(tekst);
+                        /*-------------------------------------------------*/
+                          //PREDNJI TEKST NAZIV
+                          var naziv = document.createElement('h3');
+                          naziv.classList.add('naziv');
+                          naziv.append(ime);
+                          tekst.append(naziv);
+                          //DIV IKONE
+                          var ikone = document.createElement('div');
+                          ikone.classList.add('ikone');
+                          tekst.append(ikone);
+                          /*--------------------------------------*/
+                            //P ZA IKONE
+                            var opis = document.createElement('p');
+                            opis.classList.add('opis');
+                            ikone.append(opis);
+                            //IKONE
+                            opis.innerHTML = "<i class='far fa-clock'></i>"
+                            var vrimetxt = vrime + "min";
+                            opis.append(vrimetxt);
+                            var br = document.createElement("br");
+                            opis.append(br);
+
+                            opis.innerHTML += "<i class='fas fa-seedling'></i>"
+                            var vege;
+                            if(veg=="da"){
+                              vege = "Vegetartian"
+                            }
+                            else{
+                              vege = "Non Vegetarian"
+                            };
+                            opis.append(vege);
+                            opis.append(br);
+
+                            opis.innerHTML += "<i class='fab fa-gripfire'></i>"
+                            opis.append(kalorije+"kJ");
+                            opis.append(br);
+                      //PREDNJI BOTUN
+                      var div_botun = document.createElement('div');
+                      div_botun.classList.add('botun');
+                      prednji.append(div_botun);
+                      
+                      var botun = document.createElement('button');
+                      botun.type="submit";
+                      botun.innerHTML="U kosaru";
+                      div_botun.append(botun);
+
+                      var odabrani = event.currentTarget;
+                      odabrani.parentElement.parentElement.parentElement.parentElement.parentElement.replaceChild(kartica,odabrani.parentElement.parentElement.parentElement.parentElement);
+            }
+          
+          }
+          var nadodan = new kartica(v_naziv,v_vrime,v_vege,v_kalorije,v_img);
+          
+        }
+
       }
-  var input = new inputkartica;
+    var input = new inputkartica();
 
   }
   else{
